d5ecb21643275e51fd4ab8e79cbfe5a9256dfa9c
HMRDevil <ivanmezinov@mail.ru>
2023-10-24 11:23:44 +0300

    add description CollectionInterface

--- CollectionInterface.php
+++ CollectionInterface.php
@@ -3,7 +3,10 @@
 namespace Flitework\Contracts;
 
 /**
- *
+ * Abstraction for a collection. It can be used to implement the behavior of
+ * any object containing anything, with the ability to add, delete, count what
+ * is contained inside
+ * 
  * @author Ivan Mezinov <ivanmezinov@mail.ru>
  */
 interface CollectionInterface extends \Countable, \IteratorAggregate
